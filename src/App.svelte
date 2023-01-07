ff0124f921e28d70ef2632c37ff2d7dac6300fc3
Brandon Vizcarra <branduski_4@hotmail.com>
2023-01-07 17:19:51 -0600

    Finalizado la seccion de skills y certs

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,12 @@
 import './app.css'
 import Header from './shared/Header.svelte'
 import Main from './components/Main.svelte'
+import Courses from './components/Courses.svelte';
 </script>
 
 <Header />
 <Main />
+<Courses />
 <style>
   
 </style>
